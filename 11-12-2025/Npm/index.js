aaf9cb96b795225e56f471b0d9dc417e4d96b1e7
Aswin <aswin952003@gmail.com>
2025-12-11 17:48:14 +0530

    11-12-2025 3pm to 6pm

---
+++ 11-12-2025/Npm/index.js
@@ -0,0 +1,10 @@
+// var generateName = require("sillyname");
+// var sillyname = generateName()
+
+// console.log(`My name is ${sillyname}`)
+
+import superheroes from "superheroes";
+const heroName = superheroes.random();
+
+console.log(`My name is ${heroName}!`);
+
